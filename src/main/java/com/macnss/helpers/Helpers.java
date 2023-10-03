f82c21222cdd3f3685511a57c631bf290b5b0749
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-10-03 12:04:21 +0000

    generer un nombre aléatoire pour la verification

--- src/main/java/com/macnss/helpers/Helpers.java
+++ src/main/java/com/macnss/helpers/Helpers.java
@@ -1,4 +1,11 @@
 package com.macnss.helpers;
 
+import java.security.SecureRandom;
 public class Helpers {
+
+    public static int generateCode() {
+        SecureRandom random = new SecureRandom();
+        int code = random.nextInt(900000) + 100000; // Génère un nombre aléatoire entre 100000 et 999999
+        return code;
+    }
 }
