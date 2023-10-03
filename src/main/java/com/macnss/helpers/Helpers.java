0c2f04ec5b04af594179af6e6b76cc8a7a0974d0
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-10-03 12:24:55 +0000

    restruct the class

--- src/main/java/com/macnss/helpers/Helpers.java
+++ src/main/java/com/macnss/helpers/Helpers.java
@@ -1,11 +1,45 @@
 package com.macnss.helpers;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.security.SecureRandom;
+import java.util.Properties;
+
 public class Helpers {
 
     public static int generateCode() {
         SecureRandom random = new SecureRandom();
         int code = random.nextInt(900000) + 100000; // Génère un nombre aléatoire entre 100000 et 999999
         return code;
+    }
+
+    public static Boolean sendMail(String body,String subject ,String email) {
+        final String username = "amina.el.hakik2@gmail.com";
+        final String password = "iryzwtvdahnvechw";
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setText(body);
+            Transport.send(message);
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
