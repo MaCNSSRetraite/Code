67925403bb4993d5e0f1ef880d5ec7a093196b0e
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-10-06 04:06:36 +0000

    calcul retrait

--- src/main/java/com/macnss/helpers/CalculRetraite.java
+++ src/main/java/com/macnss/helpers/CalculRetraite.java
@@ -1,9 +1,29 @@
 package com.macnss.helpers;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CalculRetraite {
+
+    // Lambda function to calculate the percentage based on salaire and totaleJourTravail
+    private static BiFunction<Float, Integer, Double> calculatePercentage = (salaire, totaleJourTravail) -> {
+        if (totaleJourTravail <= 3240) {
+            return 50 / 100.0;
+        } else if (totaleJourTravail >= 7560) {
+            return 70 / 100.0;
+        } else {
+            int nombreDeFoisDe216 = (totaleJourTravail - 3240) / 216;
+            return (50 + nombreDeFoisDe216) / 100.0;
+        }
+    };
+
+
+    public static double calculePrixRetraite(float salaire, int totaleJourTravail) {
+        double percentage = calculatePercentage.apply(salaire, totaleJourTravail);
+        double prixRetraiteParMois = salaire * percentage;
+        return contSalaireRetreite((float) prixRetraiteParMois);
+    }
 
     /*public static int calculerNombreMois(int nombreDeJours) {
         final double JOURS_PAR_MOIS = 26;
@@ -27,7 +47,7 @@ public class CalculRetraite {
 
 
 
-    public static double calculePrixRetraite(float salaire, int totaleJourTravail){
+    /*public static double calculePrixRetraite(float salaire, int totaleJourTravail){
         if (totaleJourTravail<=3240) {
             Supplier<Double> calculprixRetraiteParMois = () ->
                     salaire * (50 / 100.0);
@@ -49,5 +69,5 @@ public class CalculRetraite {
             prixRetraiteParMois = contSalaireRetreite((float) prixRetraiteParMois);
             return prixRetraiteParMois;
         }
-    }
+    }*/
 }
